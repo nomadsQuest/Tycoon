c65fcee2b44032ff5518c4da49d7b2635be35335
Dane <nicofedl98@gmail.com>
2019-07-25 21:06:53 +0200

    farming mill

---
+++ Tycoon/Assets/EnterMill.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.SceneManagement;
+
+public class EnterMill : MonoBehaviour {
+
+	public Transform player;
+	public Text EnterBuildingText;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.Find ("player").transform;
+		EnterBuildingText.enabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float dist = Vector3.Distance (transform.position, player.position);
+		//print (dist);
+
+		if (dist < 8f) 
+		{
+			EnterBuildingText.text = ("press E to Enter");
+			EnterBuildingText.enabled = true;
+
+			if (Input.GetKey ("g")) 
+			{
+				print ("Changing Level");
+
+
+			}
+		} else
+		{
+			EnterBuildingText.enabled = false;
+		}
+		
+			
+	}
+}
